bdda62019ececa16f95bc89803a9609249a9a3ad
Daryl Richter <daryl@deliverycircle.com>
2021-01-20 09:13:38 -0500

    can login. w00t!

--- lib/urbit/api.rb
+++ lib/urbit/api.rb
@@ -2,7 +2,25 @@ require "urbit/api/version"
 
 module Urbit
   module Api
-    class Error < StandardError; end
-    # Your code goes here...
+    class Config
+      def initialize
+        require 'yaml'
+        config_file = YAML.load_file('_config.yml.example')
+        @pier_code = config_file['code']
+        @pier_name = config_file['pier']
+      end
+
+      def pier_code
+        @pier_code
+      end
+
+      def pier_name
+        @pier_name
+      end
+    end
+
+    class Error < StandardError;
+    end
+
   end
 end
