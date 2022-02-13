a6313d4094e09bec12e01cf3dd5daaf8e982410e
Daryl Richter <daryl@ngzax.com>
2022-02-12 20:14:26 -0500

    can now "archive" or delete a group

--- lib/urbit/group_manager.rb
+++ lib/urbit/group_manager.rb
@@ -17,11 +17,16 @@ module Urbit
     end
 
     def create(name:, title:, description:)
-      data = %Q({"create": {"name": "#{name}", "title": "#{title}", "description": "#{description}", "policy": {"open": {"banRanks": [], "banned": []}}}})
-      self.ship.spider(mark_in: 'group-view-action', mark_out: 'json', thread: 'group-create', data: data)
-      nil
+      self.spider('group-create', %Q({"create": {"name": "#{name}", "title": "#{title}", "description": "#{description}", "policy": {"open": {"banRanks": [], "banned": []}}}}))
     end
 
+    #
+    # This is the action labeled as "Archive" in the Landscape UI.
+    def delete(group:)
+      spdr = self.spider('group-delete', %Q({"remove": {"ship": "#{group.host}", "name": "#{group.key}"}}))
+      self.remove(group) if 200 == spdr[:status]
+      nil
+    end
 
     def empty?
       self.groups.empty?
@@ -30,7 +35,7 @@ module Urbit
     #
     # Answers the Group uniquely keyed by path:, if it exists
     #
-    def find_by_path(group_path)
+    def find(path:)
       self.groups.select {|g| g.path == group_path}.first
     end
 
@@ -39,15 +44,14 @@ module Urbit
     end
 
     def join(host:, name:, share_contact: false, auto_join: false)
-      poke_msg = {join: {resource: {ship: "#{host}", name: "#{name}"}, ship: "#{host}", shareContact: share_contact, app: "groups", autojoin: auto_join}}
-      self.ship.poke(app: 'group-view', mark: 'group-view-action', message: poke_msg)
+      data = {join: {resource: {ship: "#{host}", name: "#{name}"}, ship: "#{host}", shareContact: share_contact, app: "groups", autojoin: auto_join}}
+      self.ship.poke(app: 'group-view', mark: 'group-view-action', message: data)
       nil
     end
 
     def leave(group:)
-      data = %Q({"leave": {"ship": "#{group.host}", "name": "#{group.key}"}})
-      self.ship.spider(mark_in: 'group-view-action', mark_out: 'json', thread: 'group-leave', data: data)
-      @groups = self.groups.filter {|g| g != group}
+      spdr = self.spider('group-leave', %Q({"leave": {"ship": "#{group.host}", "name": "#{group.key}"}}))
+      self.remove(group) if 200 == spdr[:status]
       nil
     end
 
@@ -64,6 +68,16 @@ module Urbit
 
     def to_s
       self.list.sort.each {|g| puts g}
+    end
+
+    private
+
+    def remove(group)
+      @groups = self.groups.filter {|g| g != group}
+    end
+
+    def spider(thread, data)
+      self.ship.spider(mark_in: 'group-view-action', mark_out: 'json', thread: thread, data: data)
     end
   end
 end
